563e84d0d8a531cb6212391f4ad33688489c06b4
ctdevJS <>
2020-01-23 13:56:37 +0100

    Done!

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Ball from './Ball';
+import Lottery from './Lottery';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Lottery />
+        <Lottery title= 'Mini dayly' maxNum={10} numBalls={4} />
+      </div>
+    );
+  }
+}
+
+export default App;
